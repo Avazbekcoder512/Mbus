b460a2925566bdf028d19a1f3e45979b4a55201c
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-04-09 12:41:45 +0500

    test

--- api/models/seat.js
+++ api/models/seat.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const seatSchema = new mongoose.Schema({
     seatNumber: Number,
+    departure_date: String,
     bus: { type: mongoose.Schema.Types.ObjectId, ref: "Bus" },
     status: { type: String, enum: ["bo'sh", "band"], default: "bo'sh" },
     booked_by: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
