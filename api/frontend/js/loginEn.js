e91e1611210f8e615760982958ec9b0c81721f31
Avazbek Qalandarov <qalandarovavazbek43@gmail.com>
2025-05-31 17:53:34 +0500

    test

--- api/frontend/js/loginEn.js
+++ api/frontend/js/loginEn.js
@@ -23,18 +23,20 @@ let changeForm = e => {
     aContainer.classList.toggle("is-txl");
     bContainer.classList.toggle("is-txl");
     bContainer.classList.toggle("is-z200");
+    aContainer.classList.toggle("is-hidden");
+    bContainer.classList.toggle("is-hidden");
 };
 
-let mainF = e => {
+let mainF = () => {
     allButtons.forEach(btn => btn.addEventListener("click", getButtons));
     switchBtn.forEach(btn => btn.addEventListener("click", changeForm));
 
-    // On page load, show the login form:
+    // Sahifa yuklanganda login old tomonda bo‘lsin:
     switchCtn.classList.add("is-txr");
     switchCircle.forEach(c => c.classList.add("is-txr"));
     switchC1.classList.add("is-hidden");
     switchC2.classList.remove("is-hidden");
-    aContainer.classList.add("is-txl");
+    aContainer.classList.add("is-txl", "is-hidden");
     bContainer.classList.add("is-txl", "is-z200");
 };
 
